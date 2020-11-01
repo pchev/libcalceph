b3ab8d584d31d694dd1a1861e560326876d39c36
Patrick Chevalley <pch@ap-i.net>
2020-11-01 22:05:17 +0100

    Install minimal doc

--- install.sh
+++ install.sh
@@ -10,8 +10,13 @@ echo Install libcalceph to $destdir
 
 install -m 755 -d $destdir
 install -m 755 -d $destdir/lib
+install -m 755 -d $destdir/share
+install -m 755 -d $destdir/share/doc
+install -m 755 -d $destdir/share/doc/libcalceph1
 
 install -v -m 644 -s calceph/libcalceph.so.*  $destdir/lib/
+install -v -m 644 changelog $destdir/share/doc/libcalceph1/changelog
+install -v -m 644 copyright $destdir/share/doc/libcalceph1/copyright
 
 cd $destdir/lib
 ln -s libcalceph.so.1.1 libcalceph.so.1
